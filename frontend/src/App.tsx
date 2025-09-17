f3c1bc3bea662d7da77c4feb1d147c3ac270e586
LeeInkyu <leeing1414@gmail.com>
2025-09-17 14:34:18 +0900

    어드민 페이지 로그인 필수하도록 변경

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import LoginPage from './LoginPage';
 import SearchPage from './SearchPage';
 import AdminPage from './AdminPage'; // Import AdminPage
+import PrivateRoute from './PrivateRoute';
 
 function App() {
   return (
@@ -10,7 +11,14 @@ function App() {
       <Routes>
         <Route path="/login" element={<LoginPage />} />
         <Route path="/search/:storeSpot" element={<SearchPage />} />
-        <Route path="/admin" element={<AdminPage />} /> {/* Add AdminPage route */}
+        <Route
+          path="/admin"
+          element={
+            <PrivateRoute>
+              <AdminPage />
+            </PrivateRoute>
+          }
+        />
         <Route path="*" element={<LoginPage />} /> {/* 기본 경로 또는 잘못된 경로 처리 */}
       </Routes>
     </BrowserRouter>
